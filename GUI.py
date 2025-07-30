d866da11910eed32a8714e454eef8c450a828bcd
Risina Surandi <risinasurandi@gmail.com>
2025-07-30 10:17:43 +0530

    Load the data

--- GUI.py
+++ GUI.py
@@ -7,3 +7,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from imblearn.over_sampling import SMOTE
+
+# 1. Load and prepare training data
+df = pd.read_csv("C:\\Users\\RISINA\\Desktop\\Thushan\\Analyzing Sail Delays (1).csv")
+df.drop(columns=['Sail ID', 'Transfer Date', 'Ship Date'], inplace=True)
